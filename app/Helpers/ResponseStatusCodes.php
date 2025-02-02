2fec788702f66edc87cab5a3d1f544f3bd239a7a
Chukwuebuka Ohaji <ebucodes@gmail.com>
2025-02-02 15:19:39 +0100

    created Helper/ResponseStatusCodes

---
+++ app/Helpers/ResponseStatusCodes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Helpers;
+
+class ResponseStatusCodes
+{
+    const SUCCESS = '200';
+
+    const INVALID_AUTH_CREDENTIAL = '400';
+    const CREATED = '201';
+    const UNAUTHORIZED = '401';
+    const  INTERNAL_SERVER_ERROR = '500';
+}
