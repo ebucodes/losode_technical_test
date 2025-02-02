e402af68d80438694e3b212e262850e6a7f76f5d
Chukwuebuka Ohaji <ebucodes@gmail.com>
2025-02-02 17:15:17 +0100

    created const for successful actions

--- app/Helpers/ResponseMessages.php
+++ app/Helpers/ResponseMessages.php
@@ -4,13 +4,15 @@ namespace App\Helpers;
 
 class ResponseMessages
 {
-    const ACTION_SUCCESSFUL = 'Action successful';
+    const ACTION_SUCCESSFUL = 'Successful';
     const ACTION_FAILED = 'Action failed';
     const LOGIN_SUCCESSFUL = 'Login successful';
-
+    const LOGOUT_SUCCESSFUL = 'Successful logged out user';
     const LOGIN_FAILED = 'Login failed';
 
+    const USER_NOT_FOUND = 'User not found';
 
+    const USER_REQUIRED = 'User ID is missing';
 
     const INVALID_AUTH_CREDENTIAL = 'Invalid login credentials';
     const UNAUTHORIZED = 'This user is not authorized to access this page';
