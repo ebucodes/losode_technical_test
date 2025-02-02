25583a699492790c24a338846315acee78c3a7e4
Chukwuebuka Ohaji <ebucodes@gmail.com>
2025-02-02 17:15:46 +0100

    added const for not found errors

--- app/Helpers/ResponseStatusCodes.php
+++ app/Helpers/ResponseStatusCodes.php
@@ -7,6 +7,8 @@ class ResponseStatusCodes
     const SUCCESS = '200';
 
     const INVALID_AUTH_CREDENTIAL = '400';
+
+    const NOT_FOUND = '404';
     const CREATED = '201';
     const UNAUTHORIZED = '401';
     const  INTERNAL_SERVER_ERROR = '500';
