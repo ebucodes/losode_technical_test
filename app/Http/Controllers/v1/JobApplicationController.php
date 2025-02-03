a46d39d559ae9e85f3c0f4887b0509dc560c29c8
Chukwuebuka Ohaji <ebucodes@gmail.com>
2025-02-03 21:29:12 +0100

    updated Job Application controller for store function

--- app/Http/Controllers/v1/JobApplicationController.php
+++ app/Http/Controllers/v1/JobApplicationController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreJobApplicationRequest;
+use App\Services\JobApplicationService;
+use Illuminate\Http\JsonResponse;
 
 class JobApplicationController extends Controller
 {
+    public function __construct(
+        protected JobApplicationService $jobApplicationService
+    ) {}
+
     //
+    public function store(StoreJobApplicationRequest $request, $job_id): JsonResponse
+    {
+        $uploadPath = public_path('uploads/cv');
+        if (!file_exists($uploadPath)) {
+            mkdir($uploadPath, 0755, true);
+        }
+        return $this->jobApplicationService->createJobApplication($job_id, $request->validated());
+    }
 }
