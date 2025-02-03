946ab8332f5bdf058cbd1812b90527ea9d5ec154
Chukwuebuka Ohaji <ebucodes@gmail.com>
2025-02-03 21:38:48 +0100

    created repository for Job application

---
+++ app/Repositories/JobApplicationRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\JobApplicationInterface;
+use App\Models\JobApplication;
+
+class JobApplicationRepository implements JobApplicationInterface
+{
+    public function __construct(protected JobApplication $model) {}
+
+    // 
+    public function create(string $job_id, array $data): JobApplication
+
+    {
+        // $data['job_id'] = $job_id;
+        // return $this->model->create($data);
+        logger(json_encode($data));
+        return $this->model->create([
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'email' => $data['email'],
+            'phone' => $data['phone'],
+            'location' => $data['location'],
+            'cv_path' => $data['cv_path'],
+            'job_id' => $job_id
+        ]);
+    }
+}
